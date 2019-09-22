b8fcd4b12534515b7fd42e814e076ee43d77396d
Sky <erniesanchez619@gmial.com>
2019-09-21 17:20:59 -0700

    match edit was not popping up

--- server.js
+++ server.js
@@ -1,5 +1,3 @@
-
-
 var express = require('express');
 var path = require('path');
 
@@ -7,16 +5,11 @@ var path = require('path');
 var app = express();
 var PORT = process.env.PORT || 8686;
 
-
-
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-
 require('./app/routing/apiRoutes.js')(app); 
 require('./app/routing/htmlRoutes.js')(app);
-
-
 
 //listener
 app.listen(PORT, function() {
